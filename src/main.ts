32d036346264981365bae2c16e2e619bdba96e8e
Adriel Morales <adriel8152@hotmail.com>
2025-05-10 18:38:48 -0600

    Se agrega Swagger al proyecto, se documentan endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ABCD Asistencia API')
+    .setDescription('API para la aplicación web de ABCD Asistencia')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen( port );
 
   console.log(`🚀 Application is running on: http://localhost:${port}`);
