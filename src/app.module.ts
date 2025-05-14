edccd504a23fa620a4a50a75a57a9ae544875b0d
Adriel Morales <adriel8152@hotmail.com>
2025-05-13 19:52:56 -0600

    Se elimina controlador y servicio de ejemplo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmpleadoModule } from './empleados/empleado.module';
 import { DepartamentoModule } from './departamentos/departamento.module';
 
 @Module({
   imports: [EmpleadoModule,DepartamentoModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
