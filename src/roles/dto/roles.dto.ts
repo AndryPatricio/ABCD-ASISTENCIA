74789e24ce79de110e7ba338d1a5234d4eda9dd9
Adriel8152 <139513716+Adriel8152@users.noreply.github.com>
2025-05-17 15:56:38 -0600

    Delete src/roles directory
    Se elimina directorio duplicado por conflicto en nombre

--- src/roles/dto/roles.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class RolDto {
-	@ApiProperty({ description: 'Id del rol', example: 0 })
-	id: string;
-
-	@ApiProperty({ description: 'Nombre del rol', example: ['asd', 'dsa'] })
-	name: string;
-}
