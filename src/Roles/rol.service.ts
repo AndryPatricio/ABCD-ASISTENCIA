fb8a19311647882735d88b3191578bbba510076a
AndryFlores <andry.patito@gmail.com>
2025-05-17 15:40:50 -0600

    Se eliminaro importaciones de roles

--- src/Roles/rol.service.ts
+++ src/Roles/rol.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { RolData } from './dto/roles.dto';
 
 @Injectable()
 export class RolService {
